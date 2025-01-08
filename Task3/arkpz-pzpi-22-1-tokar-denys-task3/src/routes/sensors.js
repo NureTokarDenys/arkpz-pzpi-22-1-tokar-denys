f4c886d151e92984213bc288925d3e091ee5c7ca
NureTokarDenys <denys.tokar@nure.ua>
2025-01-08 19:25:48 +0200

    add logging

--- Task3/arkpz-pzpi-22-1-tokar-denys-task3/src/routes/sensors.js
+++ Task3/arkpz-pzpi-22-1-tokar-denys-task3/src/routes/sensors.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Sensor = require('../models/Sensor');
+const Log = require('../models/Log');
+
 
 // GET all sensors
 router.get('/', async (req, res) => {
@@ -15,10 +17,10 @@ router.get('/', async (req, res) => {
 // GET all sensors of a specific greenhouse
 router.get('/greenhouse/:greenhouseId', async (req, res) => {
   try {
-      const sensors = await Sensor.find({ greenhouseId: req.params.greenhouseId }).populate('greenhouseId', 'name');
-      res.json(sensors);
+    const sensors = await Sensor.find({ greenhouseId: req.params.greenhouseId }).populate('greenhouseId', 'name');
+    res.json(sensors);
   } catch (err) {
-      res.status(500).json({ message: err.message });
+    res.status(500).json({ message: err.message });
   }
 });
 
@@ -29,17 +31,18 @@ router.get('/:id', getSensor, (req, res) => {
 
 // POST a new sensor
 router.post('/', async (req, res) => {
-    const sensor = new Sensor({
-        type: req.body.type, // ['temperature', 'humidity', 'light']
-        greenhouseId: req.body.greenhouseId,
-        model: req.body.model,
-        status: req.body.status, // ['active', 'inactive']
-        unit: req.body.unit,
-        lastValue: req.body.lastValue,
-        lastUpdated: req.body.lastUpdated
-    });
+  const sensor = new Sensor({
+    type: req.body.type,
+    greenhouseId: req.body.greenhouseId,
+    model: req.body.model,
+    status: req.body.status,
+    unit: req.body.unit,
+    lastValue: req.body.lastValue,
+    lastUpdated: req.body.lastUpdated,
+  });
   try {
     const newSensor = await sensor.save();
+      await logEvent(newSensor.greenhouseId, 'info', `New sensor added: ${newSensor._id}`);
     res.status(201).json(newSensor);
   } catch (err) {
     res.status(400).json({ message: err.message });
@@ -48,27 +51,27 @@ router.post('/', async (req, res) => {
 
 // PATCH an existing sensor
 router.patch('/:id', getSensor, async (req, res) => {
-    if (req.body.type != null) {
-        res.sensor.type = req.body.type;
-    }
-    if (req.body.greenhouseId != null) {
-        res.sensor.greenhouseId = req.body.greenhouseId;
-    }
-    if (req.body.model != null) {
-        res.sensor.model = req.body.model;
-    }
-    if (req.body.status != null) {
-        res.sensor.status = req.body.status;
-    }
-    if (req.body.unit != null) {
-        res.sensor.unit = req.body.unit;
-    }
-    if (req.body.lastValue != null) {
-        res.sensor.lastValue = req.body.lastValue;
-    }
-    if (req.body.lastUpdated != null) {
-        res.sensor.lastUpdated = req.body.lastUpdated;
-    }
+  if (req.body.type != null) {
+    res.sensor.type = req.body.type;
+  }
+  if (req.body.greenhouseId != null) {
+    res.sensor.greenhouseId = req.body.greenhouseId;
+  }
+  if (req.body.model != null) {
+    res.sensor.model = req.body.model;
+  }
+  if (req.body.status != null) {
+    res.sensor.status = req.body.status;
+  }
+  if (req.body.unit != null) {
+    res.sensor.unit = req.body.unit;
+  }
+  if (req.body.lastValue != null) {
+    res.sensor.lastValue = req.body.lastValue;
+  }
+  if (req.body.lastUpdated != null) {
+    res.sensor.lastUpdated = req.body.lastUpdated;
+  }
   try {
     const updatedSensor = await res.sensor.save();
     res.json(updatedSensor);
@@ -86,10 +89,11 @@ router.delete('/:id', getSensor, async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 });
+
 async function getSensor(req, res, next) {
   let sensor;
   try {
-      sensor = await Sensor.findById(req.params.id).populate('greenhouseId', 'name');
+    sensor = await Sensor.findById(req.params.id).populate('greenhouseId', 'name');
     if (sensor == null) {
       return res.status(404).json({ message: 'Cannot find sensor' });
     }
@@ -100,5 +104,20 @@ async function getSensor(req, res, next) {
   next();
 }
 
+
+async function logEvent(greenhouseId, type, message){
+    const newLog = new Log({
+        greenhouseId: greenhouseId,
+        type: type,
+        message: message,
+        timestamp: new Date(),
+    });
+    try{
+        await newLog.save()
+         console.log(`Event logged: ${message}`);
+    }catch(e){
+        console.error(`Failed to log event: ${message}, error: ${e.message}`)
+    }
+}
 
 module.exports = router;
